ea58c5ff0a8aa3311a886835edfa2032251d9fa0
Armin Shabani <batman1990ar@gmail.com>
2024-02-20 14:17:50 +0330

    Version1.0

--- extra/main.py
+++ extra/main.py
@@ -1,13 +1,16 @@
 from game import Game
 
 def main():
-    number = int(input("Enter length of the array list: "))
-    game = Game(number)
-    array = game.make_array()
-    print(array)
-    array.sort()
-    print(array)
-    return game.win_or_lose(array[-1])
-
+    while True:
+        try:
+            number = int(input("Enter length of the array list: "))
+            game = Game(number)
+            array = game.make_array()
+            print(array)
+            array.sort()
+            print(array)
+            return game.win_or_lose(array[-1])
+        except ValueError:
+            print("invalid Input")
 if __name__ == "__main__":
     print(main())
